deef1ebce8a618bd682c36b8dca283be8508808c
worrp <walterwefft@gmail.com>
2011-06-29 08:22:55 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from distutils import setup
+
+import re
+version_rx = r"^__version__ = '(.*)'$"
+version_pattern = re.compile(version_rx)
+
+
+fd = open('html2rest.py')
+try:
+    for line in fd:
+        m = version_pattern.match(line)
+        if m:
+            break
+    else:
+        raise Exception("couldn't find __version__")
+finally:
+    fd.close()
+
+__version__ = m.group(1)
+
+print "running setup for html2rest version %s" % __version__
+
+
+
+setup(
+        name="html2rest",
+        version=__version__,
+        description="Convert HTML to restructuredText",
+        author="Gerard Flanagan",
+        author_email = "grflanagan@gmail.com",
+        classifiers=["Development Status :: 4 - Beta",
+                    "Intended Audience :: Developers",
+                    "License :: OSI Approved :: BSD License",
+                    "Programming Language :: Python",
+                    "Topic :: Software Development :: Libraries",
+                    "Topic :: Software Development :: Libraries :: Python Modules",
+                    ],
+        url="http://wefft.codebasehq.com/python/html2rest",
+        license="BSD",
+        download_url="http://pypi.python.org/packages/source/h/html2rest/html2rest-%s.tar.gz" % __version__,
+        py_modules=['html2rest'],
+        install_requires=requires,
+)
+    
